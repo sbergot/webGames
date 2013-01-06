97505893378ddffde1368f4f71d78519251d8b96
Simon Bergot <simon.bergot@gmail.com>
2013-01-06 09:21:22 +0100

    add socket service and rename app

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('webGames', ['socket']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when(
+	'/tictactoe',
+	{templateUrl: 'partials/tictactoe.html', controller: MyCtrl1}
+    );
+    $routeProvider.otherwise({redirectTo: '/tictactoe'});
   }]);
